9fe434864f897f41dfd75b8e960d4f93134a728d
Derek Haas <derek.haas207@gmail.com>
2023-02-07 20:44:34 -0500

    Got play command to work

--- discord_connection.py
+++ discord_connection.py
@@ -1,26 +1,113 @@
 import os
 
 import discord
+from discord.ext import commands
+
 from dotenv import load_dotenv
 
+import youtube_dl
+
+import ctypes
+import ctypes.util
+ 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-intents = discord.Intents.default()
-intents.message_content = True
-
+intents = discord.Intents.all()
 client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='$',intents=intents)
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+youtube_dl.utils.bug_reports_message = lambda: ''
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = ""
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+        filename = data['title'] if stream else ytdl.prepare_filename(data)
+        return filename
+
+# Join discord
+@bot.command(name='join')
+async def join(ctx):
+    if not ctx.message.author.voice:
+        await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
         return
+    else:
+        channel = ctx.message.author.voice.channel
+    await channel.connect()
 
-    if message.content.startswith('$hello'):
+@bot.command(name='leave')
+async def leave(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_connected():
+        await voice_client.disconnect()
+    else:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+@bot.command(name='play')
+async def play(ctx, url):
+    
+    server = ctx.message.guild
+    voice_channel = server.voice_client
+
+    async with ctx.typing():
+        filename = await YTDLSource.from_url(url, loop=bot.loop)
+        voice_channel.play(discord.FFmpegPCMAudio(executable="/home/axyos/boyo-bot/ffmpeg", source=filename))
+    await ctx.send('**Now playing:** {}'.format(filename))
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+@bot.event
+async def on_message(message):
+
+    await bot.process_commands(message) 
+
+    if message.author == bot.user:
+        return
+    
+    if message.content.startswith('$'):
+        print('coooool')
+    
+    # Get the string the user typed
+    input = message.content.split('$')[1]
+
+    # Split the various arguments up if separated by spaces
+    # The actual command is the first argument
+    arguments = input.split(' ')
+    command = arguments[0].lower()
+
+    if command == 'hello':
         await message.channel.send('Hello!')
-
-client.run(TOKEN)
+  
+bot.run(TOKEN)
